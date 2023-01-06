7ce1268c2ffeb0627df70b71b6124b09c9fb4978
Samet Demko <demko.samet@gmail.com>
2023-01-06 11:44:21 +0100

    added publicPath

--- Package/config/webpack.prod.js
+++ Package/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
     mode: 'production', 
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/Container/latest/'
+        publicPath: '/Container/latest/',
     },
    plugins: [
     new ModuleFederationPlugin({
